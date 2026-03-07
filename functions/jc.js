1881593f60fabc03a067aab7648d10a7ca910b43
acb-site <A.Brougher@Gmail.com>
2026-03-06 17:56:33 -0800

    Update jc.js

--- functions/jc.js
+++ functions/jc.js
@@ -2,24 +2,18 @@
 const TRACKING_URL = 'https://meta-pixel-wk.adam-342.workers.dev';
 
 function triggerMetaPageView() {
-    // 1. Define the variable
     const currentFullUrl = window.location.href;
 
-    // 2. Use that SAME variable name below
     fetch(TRACKING_URL, {
-        method: 'POST',
+        method: 'GET', // GET is faster and simpler for headers
         mode: 'cors',
         headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            landing_page: currentFullUrl // This now matches the variable above
-        })
+            'x-landing-page': currentFullUrl // Send the URL here
+        }
     })
         .then(res => console.log('Meta PageView sent for:', currentFullUrl))
         .catch(err => console.error('Tracking Error:', err));
 }
-
 
 // 2. Run the function when the page is fully loaded
 if (document.readyState === 'complete') {
